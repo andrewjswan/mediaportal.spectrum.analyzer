7a2cc88cef4f576ae4b69a34dc4d15df5d3f2e68
Andrew J.Swan <ajs.blackhole@gmail.com>
2024-03-27 16:56:12 +0200

    Rework build action

--- scripts/Create_Installer.bat
+++ scripts/Create_Installer.bat
@@ -18,23 +18,8 @@ FOR /F "tokens=*" %%i IN ('Tools\sigcheck.exe /accepteula /nobanner /n "..\Spect
 :: Temp xmp2 file
 copy SpectrumAnalyzer.xmp2 SpectrumAnalyzerTemp.xmp2
 
-:: Sed "{VERSION}" from xmp2 file
-Tools\sed.exe -i "s/{VERSION}/%version%/g" SpectrumAnalyzerTemp.xmp2
-
 :: Build mpe1
 "%PROGS%\Team MediaPortal\MediaPortal\MPEMaker.exe" SpectrumAnalyzerTemp.xmp2 /B /V=%version% /UpdateXML
 
 :: Cleanup
 del SpectrumAnalyzerTemp.xmp2
-
-:: Parse version (Might be needed in the futute)
-FOR /F "tokens=1-4 delims=." %%i IN ("%version%") DO ( 
-	SET major=%%i
-	SET minor=%%j
-	SET build=%%k
-	SET revision=%%l
-)
-
-:: Rename mpe1
-if exist "..\builds\SpectrumAnalyzer-%major%.%minor%.%build%.%revision%.mpe1" del "..\builds\SpectrumAnalyzer-%major%.%minor%.%build%.%revision%.mpe1"
-rename ..\builds\SpectrumAnalyzer-MAJOR.MINOR.BUILD.REVISION.mpe1 "SpectrumAnalyzer-%major%.%minor%.%build%.%revision%.mpe1"
