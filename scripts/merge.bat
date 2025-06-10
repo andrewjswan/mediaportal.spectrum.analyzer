a3c540fe24ff0ca99b3d6499d954fc0ca5f764ca
Andrew J.Swan <ajs.blackhole@gmail.com>
2025-06-10 23:02:29 +0300

    Bump Windows to Windows Server 2022

--- scripts/merge.bat
+++ scripts/merge.bat
@@ -10,6 +10,4 @@ if "%programfiles(x86)%XXX"=="XXX" goto 32BIT
 
 if exist SpectrumAnalyzer_UNMERGED.dll del SpectrumAnalyzer_UNMERGED.dll
 ren SpectrumAnalyzer.dll SpectrumAnalyzer_UNMERGED.dll 
-rem ilmerge.exe /out:SpectrumAnalyzer.dll SpectrumAnalyzer_UNMERGED.dll Nlog.dll /target:dll /targetplatform:"v4,%PROGS%\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.0" /wildcards
-ilmerge.exe /out:SpectrumAnalyzer.dll SpectrumAnalyzer_UNMERGED.dll Nlog.dll /target:dll /targetplatform:"v4,C:\Windows\Microsoft.NET\Framework\v4.0.30319" /wildcards
-
+ilmerge.exe /out:SpectrumAnalyzer.dll SpectrumAnalyzer_UNMERGED.dll Nlog.dll /target:dll /targetplatform:"v4,%PROGS%\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.7.2" /wildcards
